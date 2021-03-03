949a1e5daf7b77d1b42bcdf9b14a6201ab23d1c5
Syntaxseed <825423+syntaxseed@users.noreply.github.com>
2021-03-03 09:04:00 -0500

    Add more word wrap CSS.

--- syntax.php
+++ syntax.php
@@ -101,8 +101,8 @@ class syntax_plugin_dokucrypt2 extends DokuWiki_Syntax_Plugin {
                   "Toggle Visible</a>]\n" .
                   "<PRE id='$curid' class='dokucrypt2pre' style=\"" .
                      (($this->curLock["collapsed"] == 1) ?
-                        "visibility:hidden;position:absolute;white-space:pre-wrap" :
-                        "visibility:visible;position:relative;white-space:pre-wrap" ) .
+                        "visibility:hidden;position:absolute;white-space:pre-wrap;word-wrap: break-word;" :
+                        "visibility:visible;position:relative;white-space:pre-wrap;word-wrap: break-word;" ) .
                   "\">".hsc($match)."</PRE>";
                 $this->curNum++;
                 break;
